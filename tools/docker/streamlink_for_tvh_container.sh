b5f0d87edf0a60d6ed9866620974027b1599ce09
cgomesu <me@cgomesu.com>
2021-04-08 10:56:15 -0300

    updated x86_64 img compatibility note

--- tools/docker/streamlink_for_tvh_container.sh
+++ tools/docker/streamlink_for_tvh_container.sh
@@ -25,6 +25,7 @@
 #
 # Tested images (tvheadend:latest):
 #  x86-64:
+#   @sha256:cf7b44ddba0bde7d1dd225cf4a6faa8f24f5cf3dd841b212b4630a4ebc9636e3
 #   @sha256:c05411d415a097b7f7cd98eef7414e09e035e6f3c740b016a6b77769f1278475
 #  arm:
 #   @sha256:d5fc6f2e77beecb655ec5325f002109cd5d5c82c9d2fc3ee39dbb4fc098cd328
